be4ff21d2da6391c79761969d3612f387e10c78a
Eric Hopper <hopper@omnifarious.org>
2017-08-05 12:55:22 -0700

    Next iteration. Factory planning list, actual production estimate.

--- factorio_calc.py
+++ factorio_calc.py
@@ -28,14 +28,23 @@ class ProductionItem:
 
     @property
     def base_rate(self):
-        base_rate = (self._produced / _F(1,1)) / (self._time / _F(1,1))
-        return base_rate
+        if self._produced is None:
+            return None
+        else:
+            base_rate = (self._produced / _F(1,1)) / (self._time / _F(1,1))
+            return base_rate
 
     def factories(self, rate):
-        return rate / self.base_rate
+        if self._produced is None:
+            return None
+        else:
+            return rate / self.base_rate
 
     def rate_with_factories(self, numfactories):
-        return numfactories * self.base_rate
+        if self._produced is None:
+            return None
+        else:
+            return numfactories * self.base_rate
 
 class ItemSet(set):
     def __init__(self, *args, **kargs):
@@ -77,7 +86,7 @@ def production_rate(dest_item, rate, source_item):
         return 0
     produced = dest_item._produced / _F(1,1)
     scale = rate / produced
-    print(f"name, scale == {dest_item._name}, {scale}")
+#    print(f"name, scale == {dest_item._name}, {scale}")
     total = 0
     for sub_item_ct, sub_item in dest_item._ingredients:
         sub_rate = production_rate(sub_item, scale * sub_item_ct, source_item)
@@ -87,3 +96,36 @@ def production_rate(dest_item, rate, source_item):
 def how_many_produced(source_item, rate, dest_item):
     forward_rate = production_rate(dest_item, _F(1,1), source_item)
     return rate / forward_rate
+
+def factories_for_each(dest_item, rate):
+    items_so_far = set()
+    factory_list = []
+    def recursive_count(dest_item, rate, cur_source=None):
+        if cur_source is None:
+            cur_source = dest_item
+        if cur_source in items_so_far:
+            return
+        items_so_far.add(cur_source)
+        source_rate = production_rate(dest_item, rate, cur_source)
+        if cur_source._produced is None:
+            factory_list.append((None, None, source_rate, cur_source))
+        else:
+            factories = cur_source.factories(source_rate)
+            int_fact = factories // _F(1,1)
+            if (factories - int_fact) > 0:
+                int_fact += 1
+            assert(int_fact >= factories)
+            factory_list.append((int_fact, factories, source_rate, cur_source))
+            for _, next_source in cur_source._ingredients:
+                recursive_count(dest_item, rate, next_source)
+    recursive_count(dest_item, rate)
+    return factory_list
+
+def actual_production(dest_item, factory_list):
+    def produced_for_each(dest_item, factory_list):
+        for int_fact, _, _, item in factory_list:
+            if int_fact is not None:
+                rate = (_F(int_fact, 1) * item._produced) / item._time
+                cur_produced = how_many_produced(item, rate, dest_item)
+                yield cur_produced
+    return min(produced_for_each(dest_item, factory_list))
