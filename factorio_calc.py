ed21d4095bde7566bd6f7d880de5a14074dbde72
Eric Hopper <hopper@omnifarious.org>
2018-02-02 10:37:30 -0800

    Added code to read and parse XML database.

--- factorio_calc.py
+++ factorio_calc.py
@@ -5,7 +5,11 @@ from fractions import Fraction as _F
 import sys
 from collections import namedtuple
 import re
+from xml.etree import ElementTree
 
+def _checkXMLHasNoText(xmlel):
+    return ((xmlel.text is None) or (xmlel.text.strip() == '')) \
+        and ((xmlel.tail is None) or (xmlel.tail.strip() == ''))
 
 class ProductionItem:
     __slots__ = ('_name', '_time', '_ingredients', '_produced', '__weakref__')
@@ -17,7 +21,6 @@ class ProductionItem:
         self._time = time
         def lookup_ingredients(ingredients):
             for ct, item in ingredients:
-                print(f"Lookup up ({ct}, {item!s})", file=sys.stderr)
                 if not isinstance(item, ProductionItem):
                     item = item_db[item]
                     print(f"Wasn't already an item, found {item!s}",
@@ -112,6 +115,67 @@ class ItemSet(set):
         for item in self:
             yield from self._itemAsXML(item, item_idmap)
         yield '</factorio_calc_item_db>\n'
+
+    @staticmethod
+    def createFromXML(infile):
+        newdb = ItemSet()
+        ET = ElementTree
+        parser = ET.XMLParser()
+        block = infile.read(4 * 1024 * 1024)
+        while len(block) > 0:
+            parser.feed(block)
+            block = infile.read(4 * 1024 * 1024)
+        block = None
+        tree = parser.close()
+        parser = None
+        if tree.tag != 'factorio_calc_item_db':
+            raise ValueError("Not an XML item database.")
+        if tree.attrib.get('version', '1.0') != '1.0':
+            raise ValueError(f"Do not know how to handle version "
+                             f"{tree.attrib['version']}.")
+        if not _checkXMLHasNoText(tree):
+            raise ValueError("Invalid XML database.")
+        item_idmap = {}
+        for itemel in tree.getchildren():
+            itemid, item = ItemSet.itemFromXML(item_idmap, itemel)
+            item_idmap[itemid] = item
+            newdb.add(item)
+        return newdb
+
+    @staticmethod
+    def itemFromXML(item_idmap, itemel):
+        if itemel.tag != 'item':
+            raise ValueError(f"Got element '{itemel.tag}', expecting 'item'.")
+        itemid = itemel.attrib['id']
+        if not _checkXMLHasNoText(itemel):
+            raise ValueError(f"Invalid item {itemid}")
+        if itemid in item_idmap:
+            raise ValueError(f"Item {itemid} defined twice.")
+        name = itemel.attrib['name']
+        time = itemel.attrib.get('time', None)
+        produced = itemel.attrib.get('produced', None)
+        if (produced is None) != (time is None):
+            raise ValueError(f"Invalid item '{itemid}'.")
+        if time is not None:
+            time = _F(time)
+            produced = int(produced)
+        ingredients = []
+        for ingredientel in itemel.getchildren():
+            if ingredientel.tag != 'ingredient':
+                raise ValueError(f"Item {itemid} has {ingredientel.tag}")
+            ingid = ingredientel.attrib['idref']
+            if not _checkXMLHasNoText(ingredientel):
+                raise ValueError(f"Invalid ingredient '{ingid}' in '{itemid}'")
+            ingcount = int(ingredientel.attrib['count'])
+            if ingid not in item_idmap:
+                raise ValueError(f"Item '{itemid}' mentions ingredient "
+                                 f"'{ingid}' before it's defined.")
+            ingredients.append((ingcount, item_idmap[ingid]))
+        if (len(ingredients) > 0) and (time is None):
+            raise ValueError(f"Item '{itemid}' has ingredients but "
+                             "no production time.")
+        return (itemid,
+                ProductionItem(name, time, tuple(ingredients), produced))
 
 _mod_dir = _osp.dirname(__file__)
 db_fname = _osp.join(_mod_dir, 'item-db.pickle')
